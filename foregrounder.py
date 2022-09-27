a111daef92436acefbbd87bd54aa36dcf7f7afd5
Devin Bayly <baylyd@email.arizona.edu>
2022-09-27 11:35:16 -0700

    switch to diff between red channel and ave of blue/green

--- foregrounder.py
+++ foregrounder.py
@@ -20,27 +20,19 @@ pth = Path(args.fname)
 
 
 def procPlant(im):
-  hsl = color.rgb2hsv(im)
   # use a histogram and the derivative of the values to figure out the min and the max 
-  hist,edges = np.histogram(im[:,:,0],bins = 256, range = (0,256))
-  d = np.diff(hist)
-  minval = np.where(d == np.max(d[100:150]))[0][0]
-  maxval = np.where(d ==np.min(d[100:250]))[0][0] + 10 # the +10 is to increase our margin of error for the upper limit of the red channel
-    #these min max are taken from image editor when a reasonable neighborhood is found
-  min_thresh = minval/255
-  max_thresh = maxval/255
-  inverted_hsl = 1-hsl
-    # we create a mask where for each pixel we say true or false if it is less than the min thresh or above the max
-    # got this part wrong the first time I typed this out.. 
-  mask = (inverted_hsl[:,:,0]>max_thresh)+(inverted_hsl[:,:,0]<min_thresh)
+  f32im= im.astype("float32")
+  diff_im = (f32im[:,:,2] + f32im[:,:,1])/2 - f32im[:,:,0]
+  mask = diff_im <-80
   masked = im.copy()
   masked[mask] =0
-  # this part has to do with auto cropping
+  ## makes sense to only check the lower 1/4, this will remove overzelous cropping
+  quarter = 3*im.shape[0]/4
   rmask=im[:,:,0] < 255 *(im[:,:,0]>200)
   gmask=im[:,:,1] < 146 *(im[:,:,1]> 45)
   bmask=im[:,:,2] < 22
   coords =np.where(rmask*gmask*bmask)
-  croppoint = np.min(coords[0])- 40 # magic number, watch out!
+  croppoint = np.min(coords[0][coords[0] > quarter])- 40 # magic number, watch out!
 
   cropped = masked[:croppoint,:,:]
 
